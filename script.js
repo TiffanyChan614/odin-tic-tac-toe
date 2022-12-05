7a6c1c4631d38124ac9db0e16263e3899ad484a4
TiffChan <tiffanychan1999614@gmail.com>
2022-12-05 13:52:08 -0500

    Refactor: generate board cells in javaScript instead of HTML

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ const GameBoard = (() => {
     let board = [['x', 'x', 'o'],
                 ['', '', 'o'],
                 ['', 'o', '']];
-    const getBoard = () => {
-        return board;
-    }
+    const getBoard = () => board;
 
+    // const move = (player) => {
+    //     const
+    //     board[row][col] = player.getMark();
+    // }
     return {getBoard};
 }) ();
 
 // DisplayController module
 const DisplayController = (() => {
     const displayBoard = (board) => {
+        const board_elem = document.querySelector(".board");
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[i].length; j++) {
+                const cell_elem = document.createElement("div");
+                cell_elem.className = "cell";
+                cell_elem.id = `cell-${i}${j}`;
+                board_elem.appendChild(cell_elem);
                 const cell = document.querySelector(`#cell-${i}${j}`);
                 cell.textContent = board[i][j];
             }
@@ -23,4 +30,17 @@ const DisplayController = (() => {
     return {displayBoard};
 }) ();
 
-DisplayController.displayBoard(GameBoard.getBoard());
+// Player factory
+const Player = (name, mark) => {
+    const getName = () => name;
+    const getMark = () => mark;
+    return {getName, getMark};
+}
+
+// GameFlow module
+const GameFlow = (() => {
+    DisplayController.displayBoard(GameBoard.getBoard());
+    let player1 = Player("Tiff", 'X');
+    let player2 = Player("Anson", 'O');
+    let currPlayer = player1;
+}) ();
