dbdf10ea86b383d23bc7074b47f9ee815478302c
TiffChan <tiffanychan1999614@gmail.com>
2022-12-05 17:51:26 -0500

    Feat: allow user to place their marks alternately

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
 // GameBoard module
 const GameBoard = (() => {
-    let board = [['x', 'x', 'o'],
-                ['', '', 'o'],
-                ['', 'o', '']];
+    let board = [['X', 'X', 'O'],
+                ['', '', 'O'],
+                ['', 'O', '']];
+
     const getBoard = () => board;
 
-    const move = (player) => {
-        const cells = document.querySelectorAll(".cell");
-        Array.from(cells).forEach(cell => {
-            cell.addEventListener('click', (e) => {
-                console.log('click', e.target);
-                let cell_num = getCellNum(e.target);
-                let row = cell_num[0];
-                let col = cell_num[1];
-                if (board[row][col] === '') {
-                    board[row][col] = player.getMark();
-                    e.target.textContent = player.getMark();
-                    DisplayController.displayBoard(GameBoard.getBoard());
+    const checkRowWin = () => {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length - 1; j++) {
+                if (board[i][j] !== board[i][j+1]) {
+                    return false;
                 }
-            });
-        })
-    }
+            }
+            return true;
+        }
+    };
 
-    const getCellNum = (cell_elem) => {
-        let id = cell_elem.id;
-        return id.replace(/[^0-9]/g, '');
-    }
+    // const checkColWin = () => {
 
-    return {getBoard, move};
+    // }
+
+    return {getBoard, checkRowWin};
+
 }) ();
 
 // DisplayController module
@@ -54,7 +49,12 @@ const DisplayController = (() => {
         }
     }
 
-    return {displayBoard, generateBoard};
+    const getCellNum = (cell_elem) => {
+        let id = cell_elem.id;
+        return id.replace(/[^0-9]/g, '');
+    };
+
+    return {displayBoard, generateBoard, getCellNum};
 }) ();
 
 // Player factory
@@ -66,10 +66,37 @@ const Player = (name, mark) => {
 
 // GameFlow module
 const GameFlow = (() => {
-    DisplayController.generateBoard(GameBoard.getBoard());
-    DisplayController.displayBoard(GameBoard.getBoard());
     let player1 = Player("Tiff", 'X');
     let player2 = Player("Anson", 'O');
-    let currPlayer = player1;
-    GameBoard.move(currPlayer);
+    let curr_player = player1;
+    let cell_clicked;
+
+    const move = () => {
+        const cells = document.querySelectorAll(".cell");
+        Array.from(cells).forEach(cell =>
+            cell.addEventListener('click', (e) => {clickEventHandler(e)}), {once: true});
+    };
+
+    const clickEventHandler = (e) => {
+        console.log('click', e.target);;
+        let cell_num = DisplayController.getCellNum(e.target);
+        let row = cell_num[0];
+        let col = cell_num[1];
+        if (GameBoard.getBoard()[row][col] === '') {
+            cell_clicked = true;
+            GameBoard.getBoard()[row][col] = curr_player.getMark();
+            e.target.textContent = curr_player.getMark();
+            DisplayController.displayBoard(GameBoard.getBoard());
+            curr_player = switchPlayer(curr_player);
+        }
+    };
+
+    const switchPlayer = (curr_player) => {
+        return curr_player === player1? player2 : player1;
+    }
+
+    DisplayController.generateBoard(GameBoard.getBoard());
+    DisplayController.displayBoard(GameBoard.getBoard());
+    move();
+
 }) ();
